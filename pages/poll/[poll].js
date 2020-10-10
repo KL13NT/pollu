190cd6d37eb558da52ff9b9a59d6c63caabe5b15
Nabil <nabil.tharwat@outlook.com>
2020-10-10 05:28:07 +0200

    feat: Poll voting page complete

---
+++ pages/poll/[poll].js
@@ -0,0 +1,146 @@
+import { server } from '../../config.routes'
+
+import Link from 'next/link'
+
+import { useState } from 'react'
+
+import { Option, Radio } from '../../src/components/Option'
+import { Button } from '../../src/components/Button'
+import { Toast, useToasts } from '../../src/components/Toast'
+import { SEO } from '../../src/components/SEO'
+import Error from '../_error'
+
+const Multiple = ({ state, onChange }) => {
+	return state.options.map(option => (
+		<Option
+			className='mt-4'
+			onChange={onChange}
+			name='selected'
+			label={option}
+			value={option}
+		>
+			{option}
+		</Option>
+	))
+}
+
+const Single = ({ state, onChange }) => {
+	return state.options.map((option, i) => (
+		<Radio
+			className='mt-4'
+			onChange={onChange}
+			name='selected'
+			value={i}
+			id={option}
+			label={option}
+		>
+			{option}
+		</Radio>
+	))
+}
+
+const PollPage = props => {
+	if (props.error) return <Error {...props} />
+
+	const [toast, setToast] = useToasts(null)
+	const [disabled, toggle] = useState(false)
+
+	const onSubmit = e => {
+		e.preventDefault()
+
+		let body = {
+			selected: null
+		}
+
+		if (props.multiple) {
+			const boxes = document.querySelectorAll('input[type="checkbox"]')
+
+			console.log(boxes)
+
+			body.selected = Array.from(boxes)
+				.filter(option => option.checked)
+				.map(option => props.options.indexOf(option.value))
+		} else {
+			body.selected = [new FormData(e.target).get('selected')]
+		}
+
+		toggle(true)
+		fetch(`/api/v1/${props._id}/vote`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(body)
+		})
+			.then(res => res.json())
+			.then(res => {
+				if (res.error) return setToast(res.error)
+				else window.location.href = `/poll/${props._id}/results`
+			})
+			.catch(err => {
+				console.log(err)
+				setToast('Something went wrong')
+			})
+			.finally(() => {
+				toggle(false)
+			})
+	}
+
+	const onSingleToggle = e => {
+		console.log(e.target)
+		return true
+	}
+
+	const onMultipleToggle = e => {
+		console.log(e.target)
+		return true
+	}
+
+	return (
+		<>
+			<SEO />
+			{toast ? <Toast>{toast}</Toast> : null}
+
+			<h1 className='mt-4 text-4xl'>{props.question}</h1>
+
+			<form className='mt-8' onSubmit={onSubmit}>
+				<fieldset disabled={disabled}>
+					{props.multiple ? (
+						<Multiple state={props} onChange={onMultipleToggle} />
+					) : (
+						<Single state={props} onChange={onSingleToggle} />
+					)}
+
+					<span className='text-left mt-4 block text-base text-alternative'>
+						{props.multiple
+							? 'You can select multiple answers'
+							: 'You can select only one answer'}
+					</span>
+
+					<Button type='submit' className='mt-4'>
+						Vote
+					</Button>
+
+					<Link href={`/poll/${props._id}/results`}>
+						<a className='mt-4 block text-base text-alternative'>
+							See results instead
+						</a>
+					</Link>
+				</fieldset>
+			</form>
+		</>
+	)
+}
+
+export async function getServerSideProps({ res, params }) {
+	const data = await (await fetch(`${server}/${params.poll}`)).json()
+
+	res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
+
+	return {
+		props: {
+			...data
+		}
+	}
+}
+export default PollPage
---
+++ pages/poll/[poll]/results.js
@@ -0,0 +1,73 @@
+import { server } from '../../../config.routes'
+
+import Link from 'next/link'
+
+import { Result } from '../../../src/components/Result'
+import { Button } from '../../../src/components/Button'
+import { useToasts, Toast } from '../../../src/components/Toast'
+import { SEO } from '../../../src/components/SEO'
+import Error from '../../_error'
+
+const Votes = ({ votes }) => {
+	if (votes > 0) return `${votes} Votes`
+
+	return 'This poll has no votes yet'
+}
+
+const ResultsPage = props => {
+	const [toast, setToast] = useToasts()
+
+	const onShare = () =>
+		navigator.clipboard
+			.writeText(window.location.href.replace('results', ''))
+			.then(() => setToast('Copied link to clipboard'))
+			.catch(() => setToast('Failed to copy link to clipboard!'))
+
+	const calcPercentage = votes => (votes ? (votes / props.votes) * 100 : 0)
+
+	if (props.error) return <Error {...props} />
+
+	return (
+		<>
+			<SEO />
+			{toast ? <Toast>{toast}</Toast> : null}
+
+			<h1 className='mt-4 text-4xl'>{props.question}</h1>
+
+			<div className='mt-8'>
+				{props.options.map(op => (
+					<Result className='mt-4' percentage={calcPercentage(op.votes)}>
+						{op.value}
+					</Result>
+				))}
+
+				<span className='text-left mt-4 block text-lg flex items-center justify-between'>
+					<Votes votes={props.votes} setToast={setToast} />
+					<Button onClick={onShare} auto className='inline-block w-auto'>
+						Share poll
+					</Button>
+				</span>
+
+				<Link href='/'>
+					<a className='mt-4 block text-base text-alternative'>
+						Create a vote instead
+					</a>
+				</Link>
+			</div>
+		</>
+	)
+}
+
+export async function getServerSideProps({ res, params }) {
+	const data = await (await fetch(`${server}/${params.poll}/results`)).json()
+
+	res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
+
+	return {
+		props: {
+			...data
+		}
+	}
+}
+
+export default ResultsPage
