b44a0736fe416bf9f457df460009e1e8e402a516
Nabil <nabil.tharwat@outlook.com>
2020-10-10 05:22:11 +0200

    refactor: replaced Gatsby SEO with Next's

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,82 +1,85 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import Head from 'next/head'
 
-function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
-
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.author || ``,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
-  )
+const defaults = {
+	sitename: 'Pollu',
+	title: 'Pollu - Delightful polls a click away!',
+	description:
+		'Pollu is an anonymous straw polls platform built by Nabil Tharwat with a focus on accessibility and privacy.'
 }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
+const Images = ({ image }) => {
+	const ogImageUrl = `${hostname}/${image}`
+
+	return (
+		<>
+			<meta content={ogImageUrl} property='og:image' />
+			<meta content={ogImageUrl} property='og:image:url' />
+			<meta content={ogImageUrl} property='og:image:secure_url' />
+			<meta content={ogImageUrl} property='twitter:image' />
+		</>
+	)
 }
 
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
+function SEO({
+	title = defaults.title,
+	description = defaults.description,
+	image
+}) {
+	const finalTitle = title ? `${title} | Pollu` : title
+
+	return (
+		<Head>
+			<title>{finalTitle}</title>
+			<meta content={finalTitle} property='og:title' />
+			<meta content={finalTitle} property='twitter:title' />
+			<meta content={description} name='description' />
+			<meta content={description} property='og:description' />
+			<meta content={description} property='twitter:description' />
+			<meta content={'image/jpeg'} property='og:image:type' />
+			<meta content='summary_large_image' name='twitter:card'></meta>
+			<meta content='website' property='og:type' />
+			<meta content={defaults.sitename} property='og:site_name' />
+			<meta content='@Nabil_Tharwat' property='twitter:creator' />
+			<meta content='@Nabil_Tharwat16' property='twitter:site' />
+
+			<link rel='manifest' href='/manifest.webmanifest' />
+
+			<meta name='msapplication-TileColor' content='#202040' />
+			<meta name='msapplication-config' content='/icons/browserconfig.xml' />
+			<meta name='theme-color' content='#202040' />
+			<link rel='shortcut icon' href='/icons/favicon.ico' />
+
+			<link
+				rel='apple-touch-icon'
+				sizes='180x180'
+				href='/icons/apple-touch-icon.png'
+			/>
+			<link
+				rel='icon'
+				type='image/png'
+				sizes='32x32'
+				href='/icons/favicon-32x32.png'
+			/>
+			<link
+				rel='icon'
+				type='image/png'
+				sizes='16x16'
+				href='/icons/favicon-16x16.png'
+			/>
+			<link
+				rel='mask-icon'
+				href='/icons/safari-pinned-tab.svg'
+				color='#5bbad5'
+			/>
+			<meta
+				name='msapplication-TileImage'
+				content='/icons/mstile-144x144.png'
+			/>
+
+			{image ? <Images image={image} /> : null}
+		</Head>
+	)
 }
 
-export default SEO
+export { SEO }
