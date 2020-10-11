54c36886c73c1d1ad34e126a6933efd379af6f1b
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-10-11 20:56:55 +0100

    Fix issue where percentage would not round to 1dp

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export function Result({ children, percentage, className, ...props }) {
+	let percentageRounded = Math.round(parseFloat(percentage) * 10)/10;
 	return (
 		<>
 			<div className={`result ${className}`} {...props}>
 				<span className='text-lg'>{children}</span>
-				<span className='text-base'>{percentage}%</span>
+				<span className='text-base'>{percentageRounded}%</span>
 				<div
 					className='absolute bottom-0 left-0 bg-progress h-1'
 					style={{ width: `${percentage}%` }}
